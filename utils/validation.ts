fab0bed8c51f6d09fe5c135e71f7e2c09f072055
Luis Dourado <lfaddourado@gmail.com>
2023-11-12 00:30:38 -0300

    fix: image formatting

--- utils/validation.ts
+++ utils/validation.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const schema = z.object({
   title: z.string().min(1).max(64),
   description: z.string().min(1).max(128),
-  type: z.enum(['png', 'jpeg', 'webp']).default('png'),
+  type: z.enum(['png', 'jpeg', 'webp']).default('jpeg'),
   dark: z
     .enum(['true', 'false'])
     .default('true')
