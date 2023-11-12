ffb9cd22ab3544309e8db4849113282acbb805c3
Luis Dourado <lfaddourado@gmail.com>
2023-11-11 22:39:21 -0300

    feat: validate and sanitize query input

---
+++ utils/validation.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+export const schema = z.object({
+  title: z.string().min(1).max(64),
+  description: z.string().min(1).max(128),
+  type: z.enum(['png', 'jpeg', 'webp']).default('png'),
+  dark: z
+    .enum(['true', 'false'])
+    .default('true')
+    .transform((val) => val === 'false'),
+});
